ef7741b08ff2cd6970b4725509de1525d76864b8
Edzus Sterns <128326488+edzussterns@users.noreply.github.com>
2023-04-28 11:02:56 +0100

    Update hello_world.py
    added f-strings in print statements

--- Task 1 - Your First Computer Program, and Using Variables/My Code/hello_world.py
+++ Task 1 - Your First Computer Program, and Using Variables/My Code/hello_world.py
@@ -1,9 +1,9 @@
 # Ask user for name and print result
-name = input("Enter your name:")
-print("Name : " + name)
+name = input("Enter your name: ")
+print(f"Name: {name}")
 
 # Ask user for age and print result
-age = input("Enter your age:")
-print("Age : " + age)
+age = input("Enter your age: ")
+print(f"Age: {age}")
 
 print("Hello World!")
