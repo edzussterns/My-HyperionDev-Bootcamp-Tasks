9695c73b58ad0749b430ed1ab97e9365e32a1a57
Edzus Sterns <128326488+edzussterns@users.noreply.github.com>
2023-04-28 11:04:09 +0100

    Delete Task 1 directory

--- Task 1/hello_world.py
+++
@@ -1,9 +0,0 @@
-# Ask user for name and print result
-name = input("Enter your name: ")
-print(f"Name: {name}")
-
-# Ask user for age and print result
-age = input("Enter your age: ")
-print(f"Age: {age}")
-
-print("Hello World!")
--- Task 1/menu.py
+++
@@ -1,9 +0,0 @@
-# Ask user to input 3 favourite foods
-item1 = input("Hello, what 3 foods would you like to order? Enter your 1st food: ")
-item2 = input("Enter your 2nd food: ")
-item3 = input("Enter your 3rd food: ")
-
-print("\nOrder confirmation! You have ordered:")
-print(item1)
-print(item2)
-print(item3)
