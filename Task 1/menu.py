e422df51e650355e6c4ef5f7ace000a0fca2ce4a
Edzus Sterns <128326488+edzussterns@users.noreply.github.com>
2023-04-28 09:23:40 +0100

    Add files via upload

---
+++ Task 1/menu.py
@@ -0,0 +1,10 @@
+# Ask user to input 3 favourite foods
+item1 = input("Hello, what 3 foods would you like to order? Enter your 1st food: ")
+item2 = input("Enter your 2nd food: ")
+item3 = input("Enter your 3rd food: ")
+
+print()
+print("Order confirmation! You have ordered:")
+print(item1)
+print(item2)
+print(item3)
