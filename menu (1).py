07abc37652a279611f3f357073cab5d654a1af70
Edzus Sterns <128326488+edzussterns@users.noreply.github.com>
2023-04-28 09:21:18 +0100

    Update menu (1).py
    Removed `print()` and added `\n` for order confirmation

--- menu (1).py
+++ menu (1).py
@@ -3,8 +3,7 @@ item1 = input("Hello, what 3 foods would you like to order? Enter your 1st food:
 item2 = input("Enter your 2nd food: ")
 item3 = input("Enter your 3rd food: ")
 
-print()
-print("Order confirmation! You have ordered:")
+print("\nOrder confirmation! You have ordered:")
 print(item1)
 print(item2)
 print(item3)
