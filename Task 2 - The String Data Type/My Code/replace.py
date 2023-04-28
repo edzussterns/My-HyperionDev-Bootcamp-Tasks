a0a8c4c4ac9258c2bfb6d5d7ccb5b3567e9491eb
Edzus Sterns <128326488+edzussterns@users.noreply.github.com>
2023-04-28 15:03:47 +0100

    Update replace.py
    Improved comment structure based on reviewer reccomendations:
    
    "Please add your comments above the code it is describing rather than inline as inline comments should be used sparingly on difficult to understand lines of code.
    
    To learn more on strings have a look here: https://www.w3schools.com/python/python_strings.asp#:~:text=Strings%20are%20Arrays,access%20elements%20of%20the%20string."

--- Task 2 - The String Data Type/My Code/replace.py
+++ Task 2 - The String Data Type/My Code/replace.py
@@ -1,9 +1,15 @@
 sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog!."
-sentence_normal = sentence.replace("!", " ") # Store sentence without "!" exclamation marks
-print(sentence_normal)  # Output: "The quick brown fox jumps over the lazy dog ."
 
-sentence_upper = sentence_normal.upper() # Store sentence in upper case
-print(sentence_upper) # Output: "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG ."
+# Store sentence without "!" exclamation marks
+sentence_normal = sentence.replace("!", " ")
+print(sentence_normal)
 
-print (sentence_upper[::-1]) # Print current state sentence in reverse: ". GOD YZAL EHT REVO SPMUJ XOF NWORB KCIUQ EHT"
-print (sentence[::-1]) # Print original sentence in reverse since task step does not clearly say which sentence needs to be printed in reverse: ".!god!yzal!eht!revo!spmuj!xof!nworb!kciuq!ehT"
+# Store sentence in upper case
+sentence_upper = sentence_normal.upper()
+print(sentence_upper)
+
+# Print current state sentence in reverse: ". GOD YZAL EHT REVO SPMUJ XOF NWORB KCIUQ EHT"
+print (sentence_upper[::-1])
+
+# Print original sentence in reverse since task step does not clearly say which sentence needs to be printed in reverse: ".!god!yzal!eht!revo!spmuj!xof!nworb!kciuq!ehT"
+print (sentence[::-1])
