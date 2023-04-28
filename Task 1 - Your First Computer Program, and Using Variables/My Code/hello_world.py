6b4ba2465b0755050f71571b5d3c842fe49e88a7
Edzus Sterns <128326488+edzussterns@users.noreply.github.com>
2023-04-28 11:01:49 +0100

    Add files via upload

---
+++ Task 1 - Your First Computer Program, and Using Variables/My Code/hello_world.py
@@ -0,0 +1,9 @@
+# Ask user for name and print result
+name = input("Enter your name:")
+print("Name : " + name)
+
+# Ask user for age and print result
+age = input("Enter your age:")
+print("Age : " + age)
+
+print("Hello World!")
