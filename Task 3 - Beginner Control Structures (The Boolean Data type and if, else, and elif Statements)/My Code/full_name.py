27b48b1f70687a3a10337e1bff522e8d76dd87b7
Edzus Sterns <128326488+edzussterns@users.noreply.github.com>
2023-04-28 14:58:05 +0100

    Update full_name.py

--- Task 3 - Beginner Control Structures (The Boolean Data type and if, else, and elif Statements)/My Code/full_name.py
+++ Task 3 - Beginner Control Structures (The Boolean Data type and if, else, and elif Statements)/My Code/full_name.py
@@ -7,12 +7,11 @@ while True:
   
   # Store the lenght of full name
   full_name_lenght = len(full_name)
-  
+
   # Check if anything has been entered at all
   if full_name_lenght == 0:
       print("You haven't entered anything. Please enter your full name.")
       continue
-  
   # Check if at least 2 words have been entered
   elif len(full_name_list) == 1:
       print("You entered less than 2 words. Please make sure that you have entered your name and surname.")
@@ -22,12 +21,10 @@ while True:
     if full_name_lenght < 4:
       print("You have entered less than 4 characters. Please make sure that you have entered your name and surname.")
       continue
-    
     # Check if entered name is too long
     elif full_name_lenght > 25:
       print("You have entered more than 25 characters. Please make sure that you have only entered your full name.")
       continue
-    
     # Exit the loop if the name is entered correctly
     else:
       print("Thank you for entering your name.")
